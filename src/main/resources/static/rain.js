2ae406ae89dfe6f32e30649df9b7d56fb435a902
ASastre03 <albertosastrezorrilla@gmail.com>
2025-03-02 17:53:22 +0100

    Spanish -> English + detalles varios

--- src/main/resources/static/rain.js
+++ src/main/resources/static/rain.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
     const rainContainer = document.querySelector('.rain');
     const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=<>?/|';
-    const numLines = 150; // Aumenté la cantidad de líneas
-    const speed = 5; // Puedes ajustar la velocidad para que sea más rápida o más lenta
+    const numLines = 150; 
+    const speed = 5; 
 
     function generateRain() {
         for (let i = 0; i < numLines; i++) {
@@ -10,21 +10,21 @@ document.addEventListener("DOMContentLoaded", function () {
             let randomText = '';
             const length = Math.floor(Math.random() * 20) + 10;
 
-            // Generar texto aleatorio
+            
             for (let j = 0; j < length; j++) {
                 randomText += chars[Math.floor(Math.random() * chars.length)];
             }
 
             line.textContent = randomText;
-            line.style.position = 'absolute';  // Necesario para posicionar las líneas correctamente
-            line.style.top = `${-Math.random() * 100}%`;  // Empieza fuera de la pantalla
-            line.style.left = `${Math.random() * 100}%`; // Distribuir aleatoriamente en el ancho
-            line.style.fontFamily = 'monospace'; // Usamos una fuente monoespaciada para un mejor efecto
-            line.style.fontSize = '15px'; // Tamaño de las letras
-            line.style.color = 'rgba(31, 231, 138, 0.92)'; // Color verde de la lluvia
-            line.style.whiteSpace = 'nowrap'; // Evitar que las líneas se rompan
+            line.style.position = 'absolute';  
+            line.style.top = `${-Math.random() * 100}%`;  
+            line.style.left = `${Math.random() * 100}%`; 
+            line.style.fontFamily = 'monospace'; 
+            line.style.fontSize = '15px'; 
+            line.style.color = 'rgba(31, 231, 138, 0.92)'; 
+            line.style.whiteSpace = 'nowrap'; 
 
-            // Añadir animación para simular la caída
+            
             line.style.animation = `fall ${Math.random() * 3 + speed}s linear infinite`;
             rainContainer.appendChild(line);
         }
--- src/main/resources/static/toggleMenu2.js
+++ src/main/resources/static/toggleMenu2.js
@@ -4,9 +4,9 @@ document.addEventListener("DOMContentLoaded", () => {
   const contentWrapper = document.querySelector(".content-wrapper");
 
   menuToggle.addEventListener("click", () => {
-    // Alterna la clase .active en la barra lateral (para ocultar/mostrar)
+    
     sidebar.classList.toggle("active");
-    // Alterna la clase .expanded en el contenedor de contenido
+    
     contentWrapper.classList.toggle("expanded");
   });
 });
