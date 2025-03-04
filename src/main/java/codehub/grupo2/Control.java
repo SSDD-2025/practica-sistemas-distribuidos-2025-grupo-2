88e8082c4c40052ea0fb4a4ee0818b3cc231aaf6
ASastre03 <albertosastrezorrilla@gmail.com>
2025-03-05 00:25:09 +0100

    init background

--- src/main/java/codehub/grupo2/Control.java
+++ src/main/java/codehub/grupo2/Control.java
@@ -220,21 +220,6 @@ public class Control implements CommandLineRunner{
             model.addAttribute("topicName", topic.get().getTopicName());
             return "postByTopic"; 
     }
-
-    /*public String showTopicPost(@PathVariable Long id, Model model) {
-        Optional<Topic> topic = TopicService.getTopicById(id);
-        if(topic == null){
-            return "redirect:/topic"; 
-        }
-        if (topic.isPresent()) {
-            List<Post> posts = PostService.getPostByTopic(topic.get());
-            model.addAttribute("posts", posts);
-            model.addAttribute("topicName", topic.get().getTopicName());
-            return "postByTopic"; 
-        } else {
-            return "redirect:/topic"; 
-        }
-    }*/
     
 
     @PostMapping("/deleteTopic")
@@ -298,47 +283,6 @@ public class Control implements CommandLineRunner{
             model.addAttribute("error", "Comment not found or deleted");
             return "redirect:/init";
         }
-    }
-
-    /* @PostMapping("/deleteComment")
-    public String deleteComment(@RequestParam long id, Model model) {
-        UserName user = userComponent.getUser();
-        if (user == null) {
-            return "redirect:/home";
-        }
-        Post p = CommentService.getCommentById(id).getPost();
-        PostService.deleteComment(p.getId(), id);
-        int comp = CommentService.deleteComment(id);
-        postComponent.getPost().getComments().remove(CommentService.getCommentById(id));
-        if(comp == 0){
-            return "redirect:/showMoreP/" + postComponent.getPost().getId();
-        }
-        else{
-            model.addAttribute("error", "Comment not found or deleted");
-            return "redirect:/init";
-        }
-    }*/
-    
-
-    @GetMapping("/editComment")//habría que eliminarlos porque no se usan
-    public String showEditComment(@RequestParam long id,  Model model) {
-        UserName user = userComponent.getUser();
-        if (user == null) {
-            return "redirect:/home";
-        }
-        Comment comment = CommentService.getCommentById(id);
-        model.addAttribute("comment", comment);
-        return "editComment";        
-    }
-    
-    @PostMapping("/editComment")//habría que eliminarlos porque no se usan
-    public String editComment(@RequestParam long id, Model model,@RequestParam String text){
-        UserName user = userComponent.getUser();
-        if (user == null) {
-            return "redirect:/home";
-        }
-        CommentService.editComment(id,text);
-        return "redirect:/acc";
     }
     
     
