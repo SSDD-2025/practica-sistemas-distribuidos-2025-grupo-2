e6ea49205d61a620b6e53eb995e5aa3000cc0fdc
Marc <francotiburon1@gmail.com>
2025-04-18 13:39:58 +0200

    Arreglado CommentMapper

--- src/main/java/codehub/grupo2/Dto/CommentMapper.java
+++ src/main/java/codehub/grupo2/Dto/CommentMapper.java
@@ -5,27 +5,25 @@ import org.mapstruct.Mapping;
 
 import codehub.grupo2.DB.Entity.Comment;
 import codehub.grupo2.DB.Entity.Post;
+import codehub.grupo2.DB.Entity.Topic;
 
-import codehub.grupo2.DB.Entity.UserName;
+// Removed unused import
 
 import java.util.Collection;
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface CommentMapper {
-
     CommentDTO toDTO(Comment Comment);
 
     List<CommentDTO> toDTOs(Collection<Comment> Comments);
 
-    @Mapping(target = "topic", ignore = true)
+    @Mapping(target = "post", ignore = true)
     Comment toDomain(CommentDTO CommentDTO);
 
-    @Mapping(target = "Comments", ignore = true)
-    Post toDomain(PostDTO PostDTO); 
+    @Mapping(target = "comments", ignore = true)
+    Post toDomain(PostDTO PostDTO);
 
-    @Mapping(target = "Comments", ignore = true)
-    UserName toDomain(UserNameDTO UserNameDTO);
 }
 
 
--- src/main/java/codehub/grupo2/Dto/UserNameMapper.java
+++ src/main/java/codehub/grupo2/Dto/UserNameMapper.java
@@ -5,12 +5,15 @@ import java.util.List;
 
 import org.mapstruct.Mapper;
 
+import codehub.grupo2.DB.Entity.Topic;
 import codehub.grupo2.DB.Entity.UserName;
 
 
 
 @Mapper(componentModel = "spring")
 public interface UserNameMapper {
+    
     UserNameDTO toDTO(UserName user);
+    
     Collection<UserNameDTO> toDTOs(List<UserName> users);
 }
